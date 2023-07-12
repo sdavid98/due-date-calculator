b9ae4dcb0773b5f43bcb8726f40569bc63419f1f
Sagi David <sagidavid98@gmail.com>
2023-07-12 08:30:30 +0200

    handle invalid input, same-day due date

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,23 @@
-describe('test', () => {
-  test('add', async () => {
-    expect(1 + 1).toEqual(2);
+import { calculateDueDate } from '.';
+
+describe('calculateDueDate', () => {
+  test('should throw error if start date is not a valid date', () => {
+    expect(() => calculateDueDate('not-a-valid-date', 1)).toThrow(
+      'Submit date is not a date!',
+    );
+  });
+
+  test('calculates same-day due dates correctly', async () => {
+    const startTime = '2023-07-12 12:34:56';
+    const turnAroundTime = 3;
+    const expected = new Date('2023-07-12 15:34:56');
+    const result = calculateDueDate(startTime, turnAroundTime);
+    expect(result).toEqual(expected);
+  });
+});
+
+describe('Timezones', () => {
+  it('should always be UTC', () => {
+    expect(new Date().getTimezoneOffset()).toBe(0);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
 console.log('Hello world!');
+
+const HOURS_IN_WORKING_DAY = 8;
+const HOUR_IN_MS = 1000 * 60 * 60;
+const DAY_IN_MS = HOUR_IN_MS * 24;
+
+const numberOfWorkDaysByHours = (hours: number) =>
+  Math.floor(hours / HOURS_IN_WORKING_DAY);
+
+const endOfDay = (date: Date) => {
+  const today = new Date(date.valueOf());
+  today.setHours(17);
+  today.setMinutes(0);
+  today.setSeconds(0);
+  today.setMilliseconds(0);
+
+  return today.valueOf();
+};
+
+const addHours = (date: Date, hours: number) =>
+  date.valueOf() + hours * HOUR_IN_MS;
+
+export const calculateDueDate = (
+  submittedAt: string,
+  turnAroundTime: number,
+) => {
+  if (isNaN(new Date(submittedAt)?.getTime())) {
+    throw 'Submit date is not a date!';
+  }
+
+  const startTime = new Date(submittedAt);
+
+  if (!turnAroundTime) {
+    return startTime;
+  }
+
+  if (addHours(startTime, turnAroundTime) <= endOfDay(startTime)) {
+    return new Date(addHours(startTime, turnAroundTime));
+  }
+};
