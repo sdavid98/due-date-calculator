39603fe402e6d6284e4403d9a1b58480adf27ccc
Sagi David <sagidavid98@gmail.com>
2023-07-12 14:20:25 +0200

    works properly with tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,6 +7,14 @@ describe('calculateDueDate', () => {
     );
   });
 
+  test('return start time if turn-around time is 0', () => {
+    const startTime = '2023-07-12 12:34:56';
+    const turnAroundTime = 0;
+    const expected = new Date(startTime);
+    const result = calculateDueDate(startTime, turnAroundTime);
+    expect(result).toEqual(expected);
+  });
+
   test('calculates same-day due dates correctly', async () => {
     const startTime = '2023-07-12 12:34:56';
     const turnAroundTime = 3;
@@ -14,6 +22,23 @@ describe('calculateDueDate', () => {
     const result = calculateDueDate(startTime, turnAroundTime);
     expect(result).toEqual(expected);
   });
+
+  test.each<[string, number, string]>([
+    ['2023-07-12 12:34:56', 5, '2023-07-13 09:34:56'],
+    ['2023-07-12 12:34:56', 10, '2023-07-13 14:34:56'],
+    ['2023-07-12 12:34:56', 80, '2023-07-26 12:34:56'],
+    ['2023-07-12 12:34:56', 85, '2023-07-27 09:34:56'],
+    ['2023-07-12 12:34:56', 200, '2023-08-16 12:34:56'],
+    ['2023-07-12 12:34:56', 202, '2023-08-16 14:34:56'],
+    ['2023-07-12 12:34:56', 206, '2023-08-17 10:34:56'],
+  ])(
+    'should calculate correct due dates across workdays and weekends',
+    (startTime, turnAroundTime, expected) => {
+      const expectedDate = new Date(expected);
+      const result = calculateDueDate(startTime, turnAroundTime);
+      expect(result).toEqual(expectedDate);
+    },
+  );
 });
 
 describe('Timezones', () => {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-console.log('Hello world!');
-
 const HOURS_IN_WORKING_DAY = 8;
 const HOUR_IN_MS = 1000 * 60 * 60;
 const DAY_IN_MS = HOUR_IN_MS * 24;
@@ -7,18 +5,61 @@ const DAY_IN_MS = HOUR_IN_MS * 24;
 const numberOfWorkDaysByHours = (hours: number) =>
   Math.floor(hours / HOURS_IN_WORKING_DAY);
 
-const endOfDay = (date: Date) => {
-  const today = new Date(date.valueOf());
-  today.setHours(17);
+const getFractalTimeInMS = (hours: number) => HOUR_IN_MS * (hours % 8);
+
+const msToNormalizedDate = (ms: number) => {
+  const today = new Date(ms);
+  today.setHours(0);
   today.setMinutes(0);
   today.setSeconds(0);
   today.setMilliseconds(0);
+
+  return today;
+};
+
+const isFriday = (date: Date) => date.getDay() === 5;
+
+const startOfDay = (date: number) => {
+  const today = msToNormalizedDate(date);
+  today.setHours(9);
+
+  return today.valueOf();
+};
+
+const endOfDay = (date: number) => {
+  const today = msToNormalizedDate(date);
+  today.setHours(17);
 
   return today.valueOf();
 };
 
 const addHours = (date: Date, hours: number) =>
   date.valueOf() + hours * HOUR_IN_MS;
+
+const addDays = (date: number, days: number) => date + days * DAY_IN_MS;
+
+const addDayShift = (date: Date, hours: number) => {
+  let daysToAdd = numberOfWorkDaysByHours(hours);
+  let addedWeekendDays = 0;
+  let weekDayOffset = date.getDay();
+  let run = true;
+
+  while (run) {
+    const totalDayDiff = weekDayOffset + daysToAdd;
+    const totalWeekendDays = Math.floor(totalDayDiff / 6) * 2;
+    const newWeekendDays = Math.max(totalWeekendDays - addedWeekendDays, 0);
+
+    daysToAdd += newWeekendDays;
+    addedWeekendDays += newWeekendDays;
+    weekDayOffset = 0;
+
+    if (newWeekendDays === 0) {
+      run = false;
+    }
+  }
+
+  return addDays(date.valueOf(), daysToAdd);
+};
 
 export const calculateDueDate = (
   submittedAt: string,
@@ -34,7 +75,22 @@ export const calculateDueDate = (
     return startTime;
   }
 
-  if (addHours(startTime, turnAroundTime) <= endOfDay(startTime)) {
+  if (addHours(startTime, turnAroundTime) <= endOfDay(startTime.valueOf())) {
     return new Date(addHours(startTime, turnAroundTime));
   }
+
+  let endDate = addDayShift(startTime, turnAroundTime);
+  let remainingTimeToAdd = getFractalTimeInMS(turnAroundTime);
+
+  if (endDate + remainingTimeToAdd > endOfDay(endDate)) {
+    const nextWorkingDayStart = isFriday(new Date(endDate))
+      ? startOfDay(addDays(endDate, 3))
+      : startOfDay(addDays(endDate, 1));
+    remainingTimeToAdd = endDate + remainingTimeToAdd - endOfDay(endDate);
+    endDate = nextWorkingDayStart + remainingTimeToAdd;
+  } else {
+    endDate += remainingTimeToAdd;
+  }
+
+  return new Date(endDate);
 };
